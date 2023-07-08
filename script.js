586b2337f04e4fad91c5cd789c18f8bdc989f49a
Usman7077 <usman.siddique707@gmail.com>
2023-07-08 20:39:08 +0500

    improve functionality after clicking equal operataor

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 let strGen = "";
-let a;
-let ans;
-let b;
+let a, b, ans;
 let dot = 0;
 let clicked = 0;
 let operator = "";
 
-document.getElementById('0').addEventListener('click', () => lowerDisplay('0'));
-document.getElementById('1').addEventListener('click', () => lowerDisplay('1'));
-document.getElementById('2').addEventListener('click', () => lowerDisplay('2'));
-document.getElementById('3').addEventListener('click', () => lowerDisplay('3'));
-document.getElementById('4').addEventListener('click', () => lowerDisplay('4'));
-document.getElementById('5').addEventListener('click', () => lowerDisplay('5'));
-document.getElementById('6').addEventListener('click', () => lowerDisplay('6'));
-document.getElementById('7').addEventListener('click', () => lowerDisplay('7'));
-document.getElementById('8').addEventListener('click', () => lowerDisplay('8'));
-document.getElementById('9').addEventListener('click', () => lowerDisplay('9'));
-document.getElementById('.').addEventListener('click', () => lowerDisplay('.'));
-document.getElementById('add').addEventListener('click', () => subt("+"));
-document.getElementById('multiply').addEventListener('click', () => subt("*"));
-document.getElementById('divide').addEventListener('click', () => subt("/"));
-document.getElementById('subtract').addEventListener('click', () => subt("-"));
-document.getElementById('equal').addEventListener('click', () => equal());
-document.getElementById('delete').addEventListener('click', () => del());
-document.getElementById('clear').addEventListener('click', () => clear());
+window.addEventListener("click", (e) => {
+    if (e.target.id >= "0" && e.target.id <= "9" || e.target.id == ".") lowerDisplay(e.target.id);
+    else if (e.target.id == "*" || e.target.id == "/" || e.target.id == "+" || e.target.id == "-") cal(e.target.id);
+    else if (e.target.id == "=") equal();
+    else if (e.target.id == "delete") del();
+    else if (e.target.id == "clear") clear();
+
+});
 
 window.addEventListener("keydown", (event) => {
     if (event.key >= "0" && event.key <= "9" || event.key == ".") lowerDisplay(event.key);
-    else if (event.key == "*" || event.key == "/" || event.key == "+" || event.key == "-") subt(event.key);
+    else if (event.key == "*" || event.key == "/" || event.key == "+" || event.key == "-") cal(event.key);
     else if (event.key == "Enter" || event.key == "=") equal();
     else if (event.key == "Backspace") del();
 });
@@ -37,17 +25,19 @@ function lowerDisplay(str) {
         dot++;
         if (dot == 1) {
             strGen += str;
+            if (strGen == ".") strGen = "0.";
             document.getElementById('lowerDisplay').innerText = strGen;
         }
     }
     else {
         strGen += str;
-        document.getElementById('lowerDisplay').innerText = strGen;
+        if (strGen.length == 1 && strGen == "0") strGen = "";
+        else document.getElementById('lowerDisplay').innerText = strGen;
     }
 }
 
-function subt(op) {
-    if (strGen == "" && a == undefined) { }
+function cal(op) {
+    if (strGen == "" && a == undefined) return
     else if (strGen == "") {
         operator = op;
         document.getElementById('upperDisplay').innerText = `${a} ${operator} `;
@@ -63,20 +53,20 @@ function subt(op) {
         }
         else {
             b = strGen;
-
             ans = calculate(a, b, operator);
             document.getElementById('lowerDisplay').innerText = ans;
             operator = op;
-            if (ans == "Cal Error!") {document.getElementById('upperDisplay').innerText = ``;}
-            else {document.getElementById('upperDisplay').innerText = `${ans} ${operator} `;}
+            if (ans == "What's this?") document.getElementById('upperDisplay').innerText = "";
+            else document.getElementById('upperDisplay').innerText = `${ans} ${operator} `;
             a = ans;
         }
         strGen = "";
     }
+    dot = 0;
 }
 
 function equal() {
-    if (strGen == "") { }
+    if (strGen == "") return;
     else {
         if (b == undefined) a = strGen; //don't calculate until b value is assigned.
         else {
@@ -84,8 +74,16 @@ function equal() {
             let totStr = `${a} ${operator} ${b} =`;
             document.getElementById('upperDisplay').innerText = totStr;
             ans = calculate(a, b, operator);
+            if (ans == "What's this?") {
+                document.getElementById('upperDisplay').innerText = "";
+                reset();
+            }
             document.getElementById('lowerDisplay').innerText = ans;
             dot = 0;
+            strGen = ans;
+            b=undefined;
+            clicked=0;
+            // strGen="";
         }
     }
 }
@@ -109,10 +107,20 @@ function clear() {
     clicked = 0;
 }
 
+function reset() {
+    strGen = "";
+    document.getElementById('upperDisplay').innerText = strGen;
+    document.getElementById('lowerDisplay').innerText = 0;
+    a = undefined;
+    b = undefined;
+    dot = 0;
+    clicked = 0;
+}
+
 const calculate = function (a, b, operator) {
     b = +b;
     a = +a;
-    if (operator == "/" && b == 0) return "Cal Error!";
+    if (operator == "/" && b == 0) return "What's this?";
     else if (operator == '+') return add(a, b);
     else if (operator == '-') return subtract(a, b);
     else if (operator == '*') return multiply(a, b);
@@ -123,4 +131,4 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-window.addEventListener("keydown", (event) => console.log(event.key));
+// window.addEventListener("click", (PointerEvent) => console.log(PointerEvent.target.getElementById));
